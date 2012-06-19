28dcd15b957bd631d8a42fc6132ea3eca9129c22
Colin Johnsun <colin.johnsun@gmail.com>
2012-06-20 00:11:22 +1000

    Add Publish-Subscribe demo.

---
+++ demo/PubSub/zpublisher.dpr
@@ -0,0 +1,50 @@
+//
+//  Pubsub envelope publisher
+//  Note that the zhelper.pas file also provides s_sendmore
+//
+//  Translated from the original C code from the ZeroMQ Guide.
+//
+program zpublisher;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils,
+  zmq,
+  zhelper;
+
+var
+  Context: Pointer;
+  Publisher: Pointer;
+  Msg: string;
+  Counter: Integer;
+begin
+  Counter := 0;
+  Context := zmq_init(1);
+
+  //  Socket to talk to clients
+
+  Publisher := zmq_socket(Context, ZMQ_PUB);
+  zmq_bind(Publisher, 'tcp://*:5563');
+
+  // Add a pause, in order to allow subscribers time to establish connection with the publisher
+  // otherwise published messages will be lost before they have a chance to subscribe to them.
+  Sleep(1000);
+
+  while True do
+  begin
+    Inc(Counter);
+    s_sendmore(Publisher, 'A');
+    s_send(Publisher, 'Only subscribers of ''A'' will see this. #' + IntToStr(Counter));
+
+    Inc(Counter);
+    s_sendmore(Publisher, 'B');
+    s_send (Publisher, 'If you subscribe to ''B'' you will see this. #' + IntToStr(Counter));
+
+    Sleep(100);
+  end;
+
+  //  We never get here but if we did, this would be how we end
+  zmq_close(Publisher);
+  zmq_term(Context);
+end.
---
+++ demo/PubSub/zsubscriber.dpr
@@ -0,0 +1,46 @@
+//
+//  Pubsub envelope subscriber
+//
+//  Translated from the original C code from the ZeroMQ Guide.
+//
+program zsubscriber;
+
+{$APPTYPE CONSOLE}
+
+uses
+  SysUtils,
+  zmq,
+  zhelper;
+
+var
+  Context: Pointer;
+  Subscriber: Pointer;
+  Key: AnsiString;
+  Topic: string;
+  Msg: string;
+
+begin
+  Key := AnsiString(ParamStr(1));
+
+  Context := zmq_init(1);
+
+  //  Socket to talk to server
+  Writeln('Connecting to hello world server');
+  Subscriber := zmq_socket(Context, ZMQ_SUB);
+  zmq_connect(Subscriber, 'tcp://localhost:5563');
+  zmq_setsockopt(Subscriber, ZMQ_SUBSCRIBE, PAnsiChar(Key), Length(Key));
+
+  while True do
+  begin
+    // Read the message topic.
+    Topic := s_recv(Subscriber);
+
+    // Read the content of the message.
+    Msg := s_recv(Subscriber);
+
+    Writeln(Format('%s - %s', [Topic, Msg]));
+  end;
+
+  zmq_close (Subscriber);
+  zmq_term (Context);
+end.
